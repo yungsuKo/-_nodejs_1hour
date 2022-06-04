27706b5c8c4178f925e87416149417287bc7462d
Tom <yungsu2391@gmail.com>
2022-06-04 16:33:02 +0900

    6/4 토 이메일 보내기 구현

--- test.js
+++ test.js
@@ -1,6 +1,30 @@
-console.log("hello, world");
+const nodemailer = require('nodemailer');
 
-let a = 5;
-let b = 3;
-let c = a + b;
-console.log(c);
+const email = {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "15aa46ce0169cf",
+      pass: "f3e363e73032ae"
+    }
+}
+
+const send = async(option) => {
+    nodemailer.createTransport(email).sendMail(option,(error,info) => {
+        if(error){
+            console.log(error);
+        }else {
+            console.log(info);
+            return info.response;
+        }
+    })
+};
+
+let email_data = {
+    from : 'yungsu2391@gmail.com',
+    to : 'yungsu2391@gmail.com',
+    subject : '테스트 메일',
+    text : 'nodejs로 한시간 만에'
+}
+
+send(email_data)
