6bcf82193c085e2c813fe9cb179d892bcd50e9b1
Tom <yungsu2391@gmail.com>
2022-06-05 00:07:41 +0900

    mysql 연결 시도

--- server.js
+++ server.js
@@ -16,3 +16,34 @@ app.get('/', function(req,res){
 app.get('/about', function(req,res){
     res.render('about.html');
 })
+
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host : 'localhost',
+    user : 'root',
+    password : '11111111',
+    database : 'opentutorials'
+});
+
+const stringfy = require('stringfy');
+
+
+app.get('/db', function(req, res){
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT 1+1 AS Solution', function (error, results, fields) {
+          // When done with the connection, release it.
+          console.log('results :', results);
+          res.send(results.Solution);
+        
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+})
